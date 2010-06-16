d804b6e39c12bc8198be4c99e1a1a4fbefbd3c58
Rolando Pereira <finalyugi@sapo.pt>
2010-06-16 01:16:29 +0100

    Write-mode now is a derived mode, derived from text-mode.
    Added the variable write-mode-left-margin that says how many spaces
    there should be on the left side of the page.

--- write-mode.el
+++ write-mode.el
@@ -1,34 +1,35 @@
-;; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with C-x C-f,
-;; then enter the text in that file's own buffer.
 
+(defvar write-mode-left-margin 5
+  "The number of spaces on the left margin of every line.")
 
-
-
-(defun write-help-fill-function ()
+(defun write-mode-fill-function ()
   (when (> (current-column) fill-column)
     (let (current-line (line-number-at-pos))
       (do-auto-fill)
-
-      ;; Sometimes `do-auto-fill' doesn't change the line
-      ;; (when (= current-line (line-number-at-pos))
-      ;;   (newline))
-
       ;; When we are on a terminal, redisplaying the frame makes the
       ;; terminal flash. I don't like that.
       ;; (quote (4)) simulates a C-u M-x recenter
       (recenter (quote (4)))
-      (indent-line-to 5)
+      (indent-line-to write-mode-left-margin)
       (move-end-of-line nil))))
 
-(setq normal-auto-fill-function 'write-help-fill-function)
+(defun write-mode-newline-recenter-indent ()
+  "Inserts a newline on the buffer, recenters the screen and indents
+the line that was just created with `write-mode-left-margin' spaces"
+  (interactive)
+  (newline)
+  (recenter)
+  (indent-line-to write-mode-left-margin))
+
+(define-derived-mode write-mode text-mode "Write"
+  "Mode that helps you write text without any distractions."
+  (setq normal-auto-fill-function 'write-mode-fill-function)
+  (setq mode-line-format nil) ; Remove the mode-line (only for this buffer)
+  (auto-fill-mode 1)
+
+  (define-key write-mode-map (kbd "RET") (lambda ()
+                                           (interactive)
+                                           (newline)
+                                           (recenter)
+                                           (indent-line-to 5))))
   
-(setq mode-line-format nil)
-
-(auto-fill-mode 1)
-
-(local-set-key (kbd "RET") (lambda ()
-                             (interactive)
-                             (newline)
-                             (recenter)
-                             (indent-line-to 5)))
